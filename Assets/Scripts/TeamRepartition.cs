f99c32928a572c7f74646576f40e165333cf7d09
GaetanPe <gaetan.peronnet@gmail.com>
2024-11-23 10:29:53 +0100

    change model wolf and sheep

--- Assets/Scripts/TeamRepartition.cs
+++ Assets/Scripts/TeamRepartition.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using UnityEngine;
 
 public class TeamRepartition : MonoBehaviour
@@ -9,55 +8,83 @@ public class TeamRepartition : MonoBehaviour
     public string role;
     public GameObject[] chickenList;
     public GameObject[] wolfList;
-    public Material chickenMaterial;
-    public Material wolfMaterial;
-    private float timeSinceLastColision = 0;
+
+    public GameObject chickenPrefab; // Prefab pour le rôle "Chicken"
+    public GameObject wolfPrefab;    // Prefab pour le rôle "Wolf"
+    private GameObject currentModel; // Le modèle actuellement utilisé
+    private SpriteRenderer spriteRenderer; // Référence au SpriteRenderer
+
+    private float timeSinceLastCollision = 0;
+
     private void Awake()
     {
-        role = roleList[Random.Range(0, roleList.Length)]; // Assign random role to Entity
-        gameObject.tag = role;
-        if (role == "Chicken")
-        {
-            gameObject.GetComponent<MeshRenderer>().material = chickenMaterial;
-        }
-        if (role == "Wolf")
-        {
-            gameObject.GetComponent<MeshRenderer>().material = wolfMaterial;
-        }
+        // Initialisation du SpriteRenderer
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 
+        // Attribuer un rôle aléatoire
+        role = roleList[Random.Range(0, roleList.Length)];
+        gameObject.tag = role;
+
+        // Appliquer le sprite correspondant
+        UpdateModel();
     }
+
     private void Update()
     {
         chickenList = GameObject.FindGameObjectsWithTag("Chicken");
         wolfList = GameObject.FindGameObjectsWithTag("Wolf");
-        timeSinceLastColision += Time.deltaTime;
+        timeSinceLastCollision += Time.deltaTime;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-
         if (collision.gameObject.tag == "Chicken")
         {
-            if (timeSinceLastColision > 5 && role == "Wolf")
+            if (timeSinceLastCollision > 5 && role == "Wolf")
             {
-                timeSinceLastColision = 0;
+                timeSinceLastCollision = 0;
 
                 Debug.Log(gameObject.name);
 
                 role = "Chicken";
-                gameObject.GetComponent<MeshRenderer>().material = chickenMaterial;
-                collision.gameObject.GetComponent<TeamRepartition>().ChickenOnColisionWithWolf();
+                UpdateModel();
+
+                collision.gameObject.GetComponent<TeamRepartition>().ChickenOnCollisionWithWolf();
 
                 gameObject.tag = role;
             }
         }
     }
 
-    public void ChickenOnColisionWithWolf()
+    public void ChickenOnCollisionWithWolf()
     {
-         role = "Wolf";
-         gameObject.GetComponent<MeshRenderer>().material = wolfMaterial;
-         gameObject.tag = role;
-         timeSinceLastColision = 0;
+        role = "Wolf";
+        UpdateModel();
+        gameObject.tag = role;
+        timeSinceLastCollision = 0;
+    }
+
+    // Méthode pour changer le sprite en fonction du rôle
+    private void UpdateModel()
+    {
+        if (currentModel != null)
+        {
+            Destroy(currentModel);
+        }
+
+        // Instancier le nouveau modèle en fonction du rôle
+        if (role == "Chicken")
+        {
+            currentModel = Instantiate(chickenPrefab, transform);
+        }
+        else if (role == "Wolf")
+        {
+            currentModel = Instantiate(wolfPrefab, transform);
+        }
+
+        // Réinitialiser la position et l'échelle
+        
+        currentModel.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+        currentModel.transform.localScale = Vector3.one;
     }
 }
